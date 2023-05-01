83af8ea0fce4f747d5c2502668ee3d45a5b354f5
Butter on Bread <qorbaxk97@gmail.com>
2023-05-01 15:05:23 +0900

    [:wrench:] 기본 페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Detail from './pages/Detail'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
-
-
   return (
     <>
-     hi
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/movies/:id" element={<Detail />} />
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import store from './redux/store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
