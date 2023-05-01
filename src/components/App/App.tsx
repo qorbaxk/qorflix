efd88e8868a99cfb10949d973a9ef264d1c23971
Butter on Bread <qorbaxk97@gmail.com>
2023-05-01 16:30:47 +0900

    [:wrench:] 페이지 추가

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import Home from '../../pages/Home'
 import Movies from '../../pages/Movies'
 import Navigation from '../../components/Navigation/Navigation'
 import Detail from '../../pages/Detail'
+import Login from '../../pages/Login'
+import Favorite from '../../pages/Favorite'
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/favorite" element={<Favorite />} />
         <Route path="/movies/:id" element={<Detail />} />
       </Routes>
     </>
