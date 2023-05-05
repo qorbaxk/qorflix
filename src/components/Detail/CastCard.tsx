a905af7becac134a4deaed234e4501cf303af654
Butter on Bread <qorbaxk97@gmail.com>
2023-05-05 19:20:43 +0900

    [:lipstick:] 이미지 없는 데이터들 대체이미지로 수정

--- src/components/Detail/CastCard.tsx
+++ src/components/Detail/CastCard.tsx
@@ -13,12 +13,27 @@ const CastCard: React.FC = () => {
     <div className="flex flex-row gap-3">
       {mainCastList.map(v => (
         <figure key={v.id}>
-          <img
-            src={`https://image.tmdb.org/t/p/w138_and_h175_face/${v.profile_path}`}
-            alt={v.name}
-            width={138}
-            height={175}
-          />
+          {v?.profile_path ? (
+            <div className="w-[138px] h-[175px]">
+              <img
+                src={`https://image.tmdb.org/t/p/w138_and_h175_face/${v.profile_path}`}
+                alt={v.name}
+                style={{
+                  objectFit: 'cover',
+                }}
+              />
+            </div>
+          ) : (
+            <div className=" w-[138px] h-[175px] flex items-center justify-center">
+              <img
+                style={{
+                  objectFit: 'cover',
+                }}
+                src="/src/assets/Person.svg"
+                alt="기본 인물 이미지"
+              />
+            </div>
+          )}
           <figcaption>
             <p>{v.name}</p>
             <p className="text-xs text-neutral-400">{v.character}</p>
--- src/components/Detail/DirectorCard.tsx
+++ src/components/Detail/DirectorCard.tsx
@@ -9,11 +9,27 @@ const DirectorCard: React.FC<DirectorProps> = ({ director }) => {
   return (
     <>
       <figure>
-        <img
-          src={`https://image.tmdb.org/t/p/w138_and_h175_face/${director?.profile_path}`}
-          alt={director?.name}
-          width={110}
-        />
+        {director?.profile_path ? (
+          <div className="w-[138px] h-[175px]">
+            <img
+              src={`https://image.tmdb.org/t/p/w138_and_h175_face/${director?.profile_path}`}
+              alt={director?.name}
+              style={{
+                objectFit: 'cover',
+              }}
+            />
+          </div>
+        ) : (
+          <div className=" w-[138px] h-[175px] flex items-center justify-center">
+            <img
+              style={{
+                objectFit: 'cover',
+              }}
+              src="/src/assets/Person.svg"
+              alt="기본 인물 이미지"
+            />
+          </div>
+        )}
         <figcaption>
           <p>{director?.name}</p>
           <p className="text-xs text-neutral-400">{director?.job}</p>
