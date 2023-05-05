a7e495c2409f654e7c7dac5e5984ae3b5b03daaa
Butter on Bread <qorbaxk97@gmail.com>
2023-05-05 16:31:09 +0900

    [:lipstick:] 별점 추가

--- src/components/Detail/OverView.tsx
+++ src/components/Detail/OverView.tsx
@@ -17,33 +17,43 @@ const OverView: React.FC = () => {
           src={`https://image.tmdb.org/t/p/original//${detail.poster_path}`}
           alt={`${detail.title} 포스터`}
           width={500}
-          // className="grow"
         />
-        <figcaption className="grow flex flex-col items-start justify-start gap-4">
-          <p className="text-base italic">{detail.tagline}</p>
-          <p className="text-5xl font-bold">{detail.title}</p>
+        <figcaption className="grow flex flex-row items-start gap-8 relative">
+          <div className="flex flex-col items-start justify-start gap-4 w-[80%]">
+            <p className="text-base italic">{detail.tagline}</p>
+            <p className="text-5xl font-bold">{detail.title}</p>
 
-          <table>
-            <tr className="text-neutral-400 text-sm flex flex-row flex-nowrap items-center">
-              <td className="lineAfter">{`${detail.release_date.substring(
-                2,
-                4,
-              )}년 ${detail.release_date.substring(
-                5,
-                7,
-              )}월 ${detail.release_date.substring(8, 10)}일`}</td>
-              <td className="lineAfter">{`${Math.floor(
-                detail.runtime / 60,
-              )}시간 ${detail.runtime % 60}분`}</td>
-              <td className="flex flex-row">
-                {detail.genres?.map(item => (
-                  <label key={item.id} className="mr-3 w-fit" aria-label="장르">
-                    {item.name}
-                  </label>
-                ))}
-              </td>
-            </tr>
-          </table>
+            <div className="absolute top-10 right-0 flex flex-row items-center gap-2">
+              <p className="text-4xl">{detail.vote_average.toFixed(1)}</p>
+              <img width={35} src="/src/assets/Star.svg" alt="별점 이미지" />
+            </div>
+
+            <table>
+              <tr className="text-neutral-400 text-sm flex flex-row flex-nowrap items-center">
+                <td className="lineAfter">{`${detail.release_date.substring(
+                  2,
+                  4,
+                )}년 ${detail.release_date.substring(
+                  5,
+                  7,
+                )}월 ${detail.release_date.substring(8, 10)}일`}</td>
+                <td className="lineAfter">{`${Math.floor(
+                  detail.runtime / 60,
+                )}시간 ${detail.runtime % 60}분`}</td>
+                <td className="flex flex-row">
+                  {detail.genres?.map(item => (
+                    <label
+                      key={item.id}
+                      className="mr-3 w-fit"
+                      aria-label="장르"
+                    >
+                      {item.name}
+                    </label>
+                  ))}
+                </td>
+              </tr>
+            </table>
+          </div>
         </figcaption>
       </figure>
     </div>
