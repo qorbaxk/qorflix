f1d92b31bcd0ecdfe3b528266c664593e12fc041
Butter on Bread <qorbaxk97@gmail.com>
2023-05-05 22:11:35 +0900

    [:sparkles:] 트레일러 api 받아오기

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react'
 import api from '../redux/api'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { getSelectedMovie } from '../redux/slice/detailSlice'
+import { RootState } from '../redux/store'
+import { getSelectedMovie, getTrailerMovie } from '../redux/slice/detailSlice'
 import { getCredits } from '../redux/slice/creditSlice'
 import OverView from '../components/Detail/OverView'
 import { trueLoading, falseLoading } from '../redux/slice/loadingSlice'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 const Detail: React.FC = () => {
   const { id } = useParams()
+  const loading = useSelector(
+    (loadingState: RootState) => loadingState.ld.loading,
+  )
+
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
   const dispatch = useDispatch()
@@ -19,22 +25,46 @@ const Detail: React.FC = () => {
     const creditMovieApi = api.get(
       `/movie/${id}/credits?api_key=${API_KEY}&language=ko-KR`,
     )
+    const trailerApi = api.get(
+      `/movie/${id}/videos?api_key=${API_KEY}&language=ko-KR`,
+    )
 
-    let [detailMovie, creditMovie] = await Promise.all([detailMovieApi, creditMovieApi])
+    let [detailMovie, creditMovie, trailerMovie] = await Promise.all([
+      detailMovieApi,
+      creditMovieApi,
+      trailerApi,
+    ])
 
     dispatch(getSelectedMovie(detailMovie.data))
     dispatch(getCredits(creditMovie.data))
+    dispatch(getTrailerMovie(trailerMovie.data.results))
     dispatch(falseLoading())
   }
 
   useEffect(() => {
     dispatch(trueLoading())
     getDetails()
+    window.scrollTo(0, 0)
   }, [])
 
+  if (loading) {
+    return (
+      <div
+        className="flex justify-center items-center bg-black min-h-screen"
+        role="alert"
+        aria-busy="true"
+        aria-live="polite"
+        aria-label="로딩중"
+      >
+        <ClipLoader color="red" loading={loading} size={150} />
+      </div>
+    )
+  }
   return (
-    <div className="baseColor baseContainer">
-      <OverView />
+    <div className='w-full bg-black'>
+      <div className="baseColor baseContainer">
+        <OverView />
+      </div>
     </div>
   )
 }
