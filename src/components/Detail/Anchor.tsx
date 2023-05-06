d07c7ffe9d34cd2b399281f170cd0ba7ba8ee7db
Butter on Bread <qorbaxk97@gmail.com>
2023-05-06 21:57:55 +0900

    [:lipstick:] 네이밍 변경

--- src/components/Detail/Anchor.tsx
+++ src/components/Detail/Anchor.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import SelectedAnchor from './SelectedAnchor'
 
 const Anchor: React.FC = () => {
-  const anchorList = ['개요', '트레일러', '리뷰', '비슷한 영화']
+  const anchorList = ['개요', '트레일러', '리뷰', '추천 영화']
   const [clicked, setClicked] = useState('개요')
 
   return (
