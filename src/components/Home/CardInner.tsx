4c902b763f72beae5996ca666d426778a5ff1418
Butter on Bread <qorbaxk97@gmail.com>
2023-05-06 21:30:22 +0900

    [:hammer:] 카드 내용 컴포넌트화화

---
+++ src/components/Home/CardInner.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from './../../redux/store'
+import { SwiperMovieProps } from './CardSwiper'
+import { useNavigate } from 'react-router-dom'
+
+type CardInnerProps = {
+  item: SwiperMovieProps
+}
+
+const CardInner: React.FC<CardInnerProps> = ({ item }) => {
+  const now = new Date()
+  const current = `${now.getFullYear()}-${
+    now.getMonth() + 1 > 9 ? now.getMonth() + 1 : `0${now.getMonth() + 1}`
+  }-${now.getDate() > 9 ? now.getDate() : `0${now.getDate()}`}`
+  const genre = useSelector((movieState: RootState) => movieState.mv.genreList)
+
+  const navigate = useNavigate()
+
+  const gotoDetail = () => {
+    navigate(`/movies/${item.id}`)
+  }
+
+  return (
+    <div onClick={gotoDetail} className='cursor-pointer'>
+      <figure>
+        <img
+          style={{
+            width: '100%',
+            height: '168.75px',
+            display: 'block',
+            objectFit: 'cover',
+            objectPosition: 'top',
+            marginBottom: '4px',
+          }}
+          src={
+            item.backdrop_path
+              ? `https://image.tmdb.org/t/p/original//${item.backdrop_path}`
+              : `https://image.tmdb.org/t/p/original//${item.poster_path}`
+          }
+        />
+        <figcaption className="flex flex-col gap-1 justify-center">
+          <h3 className="a11y-hidden">영화 제목</h3>
+          <p className="text-lg font-bold">{item.title}</p>
+          {current < item.release_date ? (
+            <p className="flex flex-row gap-2">
+              <span className="text-xs font-bold p-1 rounded-lg bg-gray-500">
+                개봉일
+              </span>
+              <span className="text-sm">{item.release_date}</span>
+            </p>
+          ) : (
+            <p className="flex flex-row gap-1">
+              <span aria-hidden>⭐</span>
+              <span role="text" aria-label="별점">
+                {item.vote_average}
+              </span>
+            </p>
+          )}
+          <div className="flex flex-row">
+            {item.genre_ids?.map((id, idx) =>
+              idx < 5 ? (
+                <label
+                  key={id}
+                  className="flex flex-row flex-nowrap text-xs font-bold p-1 w-fit dotAfter"
+                  aria-label="장르"
+                >
+                  {genre.find(item => item.id === id)?.name}
+                </label>
+              ) : null,
+            )}
+          </div>
+
+          <span
+            className={`text-xs font-bold p-1 rounded-lg w-fit ${
+              item.adult ? 'bg-red-700' : 'bg-green-700'
+            }`}
+            role="text"
+            aria-label="관람등급"
+          >
+            {item.adult ? '청소년 관람불가' : '전체관람가'}
+          </span>
+        </figcaption>
+      </figure>
+    </div>
+  )
+}
+
+export default CardInner
--- src/components/Home/CardSwiper.tsx
+++ src/components/Home/CardSwiper.tsx
@@ -5,11 +5,9 @@ import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
 
 import { EffectCoverflow, Pagination } from 'swiper'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { RootState } from './../../redux/store'
+import CardInner from './CardInner'
 
-type SwiperMovieProps = {
+export interface SwiperMovieProps {
   adult: boolean
   backdrop_path: string
   genre_ids: number[]
@@ -30,17 +28,6 @@ type SwiperState = {
 }
 
 const CardSwiper: React.FC<SwiperState> = ({ movies }) => {
-  const now = new Date()
-  const current = `${now.getFullYear()}-${
-    now.getMonth() + 1 > 9 ? now.getMonth() + 1 : `0${now.getMonth() + 1}`
-  }-${now.getDate() > 9 ? now.getDate() : `0${now.getDate()}`}`
-  const genre = useSelector((movieState: RootState) => movieState.mv.genreList)
-  const navigate = useNavigate()
-
-  const gotoDetail = (id: number) => {
-    navigate(`/movies/${id}`)
-  }
-
   return (
     <>
       <Swiper
@@ -75,68 +62,12 @@ const CardSwiper: React.FC<SwiperState> = ({ movies }) => {
               height: '300px',
             }}
             key={item.id}
-            onClick={() => gotoDetail(item.id)}
           >
-            <figure>
-              <img
-                style={{
-                  width: '100%',
-                  height: '168.75px',
-                  display: 'block',
-                  objectFit: 'cover',
-                  objectPosition: 'top',
-                  marginBottom: '4px',
-                }}
-                src={
-                  item.backdrop_path
-                    ? `https://image.tmdb.org/t/p/original//${item.backdrop_path}`
-                    : `https://image.tmdb.org/t/p/original//${item.poster_path}`
-                }
-              />
-              <figcaption className="flex flex-col gap-1 justify-center">
-                <h3 className="a11y-hidden">영화 제목</h3>
-                <p className="text-lg font-bold">{item.title}</p>
-                {current < item.release_date ? (
-                  <p className="flex flex-row gap-2">
-                    <span className="text-xs font-bold p-1 rounded-lg bg-gray-500">
-                      개봉일
-                    </span>
-                    <span className="text-sm">{item.release_date}</span>
-                  </p>
-                ) : (
-                  <p className="flex flex-row gap-1">
-                    <span aria-hidden>⭐</span>
-                    <span role="text" aria-label="별점">
-                      {item.vote_average}
-                    </span>
-                  </p>
-                )}
-                <div className="flex flex-row">
-                  {item.genre_ids?.map((id, idx) =>
-                    idx < 5 ? (
-                      <label
-                        key={id}
-                        className="flex flex-row flex-nowrap text-xs font-bold p-1 w-fit dotAfter"
-                        aria-label="장르"
-                      >
-                        {genre.find(item => item.id === id)?.name}
-                      </label>
-                    ) : null,
-                  )}
-                </div>
-
-                <span
-                  className={`text-xs font-bold p-1 rounded-lg w-fit ${
-                    item.adult ? 'bg-red-700' : 'bg-green-700'
-                  }`}
-                  role="text"
-                  aria-label="관람등급"
-                >
-                  {item.adult ? '청소년 관람불가' : '전체관람가'}
-                </span>
-              </figcaption>
-            </figure>
-            <label aria-label="순위" className="absolute bottom-0 right-0 text-gray-400 text-4xl font-black">
+            <CardInner item={item} />
+            <label
+              aria-label="순위"
+              className="absolute bottom-0 right-0 text-gray-400 text-4xl font-black"
+            >
               {idx - 1 <= 0 ? movies.length + (idx - 1) : idx - 1}
             </label>
           </SwiperSlide>
