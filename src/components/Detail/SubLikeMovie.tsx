6392cdeee100d01ed43aa2f5c7cf6d88f8c38695
Butter on Bread <qorbaxk97@gmail.com>
2023-05-06 21:55:26 +0900

    [:sparkles:] 추천영화 필터링 추가

--- src/components/Detail/SubLikeMovie.tsx
+++ src/components/Detail/SubLikeMovie.tsx
@@ -3,15 +3,18 @@ import { RootState } from '../../redux/store'
 import { useSelector } from 'react-redux'
 import CardInner from '../Home/CardInner'
 
-const SubLikeMovie = () => {
+const SubLikeMovie: React.FC = () => {
   let recommend = useSelector(
     (detailState: RootState) => detailState.dt.recommendMovieInfo,
   )
 
-  console.log(recommend)
+  recommend = recommend
+    .filter(value => value.backdrop_path !== null && value.vote_average > 5)
+    .sort((a, b) => b.vote_average - a.vote_average)
+    .splice(0, 10)
 
   return (
-    <div className="grid grid-cols-2 auto-rows-min gap-4">
+    <div className="grid grid-cols-2 auto-rows-min gap-4 mb-6">
       {recommend.map(item => (
         <CardInner key={item.id} item={item} />
       ))}
