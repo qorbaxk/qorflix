c1a06c3737bee35fe7985afa964ccf9b95f3b737
Butter on Bread <qorbaxk97@gmail.com>
2023-05-08 17:01:19 +0900

    [:sparkles:] 리뷰 전체는 말줄임표로 작게보이게 하고 클릭시 전체를 보이도록 추가

--- src/components/Detail/ReviewCard.tsx
+++ src/components/Detail/ReviewCard.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RootState } from '../../redux/store'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
 import 'moment/locale/ko'
+import { reviewProps } from '../../redux/slice/detailSlice'
 
 const ReviewCard: React.FC = () => {
+  const [selected, setSelected] = useState<reviewProps>()
   let reviews = useSelector(
     (detailState: RootState) => detailState.dt.reviewMovieInfo,
   )
@@ -12,38 +14,95 @@ const ReviewCard: React.FC = () => {
   moment.locale('ko')
   const recentReview = [...reviews].reverse()
 
+  const moreReviewHandler = (id: string) => {
+    setSelected(recentReview.find(v => v.id === id))
+  }
+
   return (
-    <div className="mb-6">
-      {recentReview.map(item => (
-        <div key={item.id} className="bg-zinc-400 text-black rounded p-4 mb-4">
-          <div className="flex flex-row items-center gap-2">
-            {item.author_details.avatar_path ? (
+    <>
+      {selected !== undefined ? (
+        <div className="bg-white text-black rounded p-4 mb-6 w-full h-fit relative">
+          <div className="flex flex-row items-center gap-2 mb-4">
+            {selected.author_details.avatar_path ? (
               <img
                 src={
-                  item.author_details.avatar_path.includes('https')
-                    ? `${item.author_details.avatar_path.slice(1)}`
-                    : `https://image.tmdb.org/t/p/w32_and_h32_face/${item.author_details.avatar_path}`
+                  selected.author_details.avatar_path.includes('https')
+                    ? `${selected.author_details.avatar_path.slice(1)}`
+                    : `https://image.tmdb.org/t/p/w32_and_h32_face/${selected.author_details.avatar_path}`
                 }
-                alt={`${item.author_details.username} 프로필 사진`}
+                alt={`${selected.author_details.username} 프로필 사진`}
                 className="rounded-full"
-                width={32}
-                height={32}
+                width={25}
+                height={25}
               />
             ) : (
               <img
-                width={32}
-                height={32}
+                width={25}
+                height={25}
                 src="/src/assets/Person.svg"
                 alt="기본 인물 이미지"
               />
             )}
-            <p className="text-lg font-bold">{item.author}</p>
+            <p className="font-bold">{selected.author}</p>
           </div>
-          <p className="my-1 text-justify text-sm">{item.content}</p>
-          <p className="text-right">{moment(item.created_at).format('LLL')}</p>
+          <p className="mb-2 text-justify text-sm">{selected.content}</p>
+          <p className="text-right text-xs">
+            {moment(selected.created_at).format('LLL')}
+          </p>
+          <button
+            onClick={() => setSelected(undefined)}
+            type="button"
+            className="border border-1 border-black p-2 m-2 absolute top-0 right-0 hover:bg-black hover:text-white"
+          >
+            닫기
+          </button>
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="mb-6 grid grid-cols-2 place-items-center gap-2 cursor-pointer">
+          {recentReview.map(item => (
+            <div
+              key={item.id}
+              onClick={() => moreReviewHandler(item.id)}
+              className="bg-zinc-400 text-black rounded p-4 w-full h-full"
+            >
+              <div className="flex flex-row items-center gap-2">
+                {item.author_details.avatar_path ? (
+                  <img
+                    src={
+                      item.author_details.avatar_path.includes('https')
+                        ? `${item.author_details.avatar_path.slice(1)}`
+                        : `https://image.tmdb.org/t/p/w32_and_h32_face/${item.author_details.avatar_path}`
+                    }
+                    alt={`${item.author_details.username} 프로필 사진`}
+                    className="rounded-full"
+                    width={25}
+                    height={25}
+                  />
+                ) : (
+                  <img
+                    width={25}
+                    height={25}
+                    src="/src/assets/Person.svg"
+                    alt="기본 인물 이미지"
+                  />
+                )}
+                <p className="font-bold">{item.author}</p>
+              </div>
+              <p
+                className={`mb-2 text-justify text-sm ${
+                  item.content.length > 65 ? 'line-clamp-2' : ''
+                }`}
+              >
+                {item.content}
+              </p>
+              <p className="text-right text-xs">
+                {moment(item.created_at).format('LLL')}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
