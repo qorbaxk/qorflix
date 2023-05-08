a5f3597862ad0b710309fae961bf65aa006dbe5f
Butter on Bread <qorbaxk97@gmail.com>
2023-05-08 22:20:04 +0900

    [:sparkles:] 올타임 영화 불러오기

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import api from '../redux/api'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import { trueLoading, falseLoading } from '../redux/slice/loadingSlice'
+import ClipLoader from 'react-spinners/ClipLoader'
+import { getGenreList, getAllTimeMovies } from '../redux/slice/movieSlice'
+import Paging from '../components/Pagination/Paging'
+import Search from '../components/Movies/Search'
+import MovieView from '../components/Movies/MovieView'
 
-const Movies = () => {
+const Movies: React.FC = () => {
+  const loading = useSelector(
+    (loadingState: RootState) => loadingState.ld.loading,
+  )
+  const page = useSelector((pageState: RootState) => pageState.pg.page)
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+  const dispatch = useDispatch()
+
+  const getAllMovies = async () => {
+    dispatch(trueLoading())
+
+    const allTimeMovieApi = api.get(
+      `/discover/movie?api_key=${API_KEY}&language=ko-KR&sort_by=revenue.desc&include_adult=true&include_video=false&page=${page}`,
+    )
+    const genreApi = api.get(
+      `/genre/movie/list?api_key=${API_KEY}&language=ko-KR`,
+    )
+
+    let [allTimeMovies, genreList] = await Promise.all([
+      allTimeMovieApi,
+      genreApi,
+    ])
+    dispatch(
+      getGenreList({
+        genreList: genreList.data.genres,
+      }),
+    )
+
+    dispatch(
+      getAllTimeMovies({
+        allTimeMovies: allTimeMovies.data.results,
+      }),
+    )
+
+    dispatch(falseLoading())
+  }
+
+  useEffect(() => {
+    getAllMovies()
+    window.scrollTo(0, 0)
+  }, [page])
+
+  if (loading) {
+    return (
+      <div
+        className="flex justify-center items-center bg-black min-h-screen"
+        role="alert"
+        aria-busy="true"
+        aria-live="polite"
+        aria-label="로딩중"
+      >
+        <ClipLoader color="red" loading={loading} size={150} />
+      </div>
+    )
+  }
   return (
-    <div>Movies</div>
+    <div className="baseColor baseContainer">
+      <div className="flex flex-row justify-around">
+        <Search />
+        <MovieView/>
+      </div>
+      <Paging />
+    </div>
   )
 }
 
--- src/redux/slice/movieSlice.tsx
+++ src/redux/slice/movieSlice.tsx
@@ -24,6 +24,7 @@ export interface MovieState {
   popularMovies: oneMovieProps[]
   upComingMovies: oneMovieProps[]
   nowPlayingMovies: oneMovieProps[]
+  allTimeMovies: oneMovieProps[]
   genreList: genreProps[]
 }
 
@@ -82,6 +83,24 @@ const initialState: MovieState = {
       vote_count: 0,
     },
   ],
+  allTimeMovies: [
+    {
+      adult: false,
+      backdrop_path: '',
+      genre_ids: [0],
+      id: 0,
+      original_language: '',
+      original_title: '',
+      overview: '',
+      popularity: 0,
+      poster_path: '',
+      release_date: '',
+      title: '',
+      video: false,
+      vote_average: 0,
+      vote_count: 0,
+    },
+  ],
   genreList: [
     {
       id: 0,
@@ -106,6 +125,9 @@ export const movieSlice = createSlice({
     getGenreList: (state, action) => {
       state.genreList = action.payload.genreList
     },
+    getAllTimeMovies: (state, action) => {
+      state.allTimeMovies = action.payload.allTimeMovies
+    },
   },
 })
 
@@ -113,6 +135,7 @@ export const {
   getMainMovies,
   getComingMovies,
   getPlayingMovies,
+  getAllTimeMovies,
   getGenreList,
 } = movieSlice.actions
 export default movieSlice.reducer
