9dd5318186695d9bd40ee1780080a8d26d8b7169
Butter on Bread <qorbaxk97@gmail.com>
2023-05-09 18:34:59 +0900

    [:sparkles:] firebase 설치 후 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import App from './components/App/App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.tsx'
+import { app } from './firebase-config.tsx'
+
+console.log(app)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
