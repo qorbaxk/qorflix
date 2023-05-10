426da97a87223c9e25c1525a12dabcf0f5b75164
Butter on Bread <qorbaxk97@gmail.com>
2023-05-10 15:56:04 +0900

    [:hammer:] 필요없는 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import App from './components/App/App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.tsx'
-import { app } from './firebase-config.tsx'
-
-console.log(app)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
