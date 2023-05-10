6f08768fba8be1d46a274d2ddf80bc0bcd05a3fe
Butter on Bread <qorbaxk97@gmail.com>
2023-05-10 15:58:55 +0900

    [:sparkles:] 마이페이지에 로그아웃 기능 추가

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { getAuth } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Mypage = () => {
+  const navigate = useNavigate()
+
+  const gotoLogout = () => {
+    const auth = getAuth()
+    auth.signOut()
+    navigate('/')
+  }
+
   return (
-    <div>Mypage</div>
+    <div>
+      <button
+        role="button"
+        onClick={gotoLogout}
+        aria-label="로그아웃 하기"
+        className="cursor-pointer"
+      >
+        로그아웃
+      </button>
+    </div>
   )
 }
 
