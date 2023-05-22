b291ad873401d15127f2da2579ffb9bafbea634d
Butter on Bread <qorbaxk97@gmail.com>
2023-05-22 16:23:50 +0900

    [:sparkles:] 찜한 영화 FB에서 불러와 렌더링

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { dbService } from '../firebase-config'
+import { getAuth } from 'firebase/auth'
+import GridCard from '../components/Favorite/GridCard'
+
+type listProps = { id: string }[]
 
 const Favorite = () => {
+  const auth = getAuth()
+  const [myLists, setMyLists] = useState<listProps>([])
+  const getList = () => {
+    if (auth.currentUser) {
+      const q = query(collection(dbService, auth.currentUser.uid))
+      onSnapshot(q, snapshot => {
+        const myListArr = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        setMyLists(myListArr)
+      })
+    }
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
   return (
-    <div>Favorite</div>
+    <div className="baseColor baseContainer px-32">
+      <h2 className="a11y-hidden">찜한 영화</h2>
+      <GridCard movie={myLists} />
+    </div>
   )
 }
 
