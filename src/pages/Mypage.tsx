cbe220a579b17af6db37a0e3d8d3fad223a02555
Butter on Bread <qorbaxk97@gmail.com>
2023-05-22 13:07:03 +0900

    [:lipstick:] 마이페이지 렌더링

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Logout from '../components/Mypage/Logout'
 import Unsubscribing from '../components/Mypage/Unsubscribing'
+import EditProfile from './../components/Mypage/EditProfile'
 
-const Mypage = () => {
+const Mypage: React.FC = () => {
   return (
-    <div className='baseColor baseContainer flex flex-col items-center gap-4'>
+    <div className="baseColor baseContainer flex flex-col items-center gap-4">
+      <EditProfile />
       <Logout />
-      <Unsubscribing/>
+      <Unsubscribing />
     </div>
   )
 }
