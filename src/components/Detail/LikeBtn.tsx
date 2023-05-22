5f4b99c5350defec7a5888363de8fd0b1d79301c
Butter on Bread <qorbaxk97@gmail.com>
2023-05-22 15:24:09 +0900

    [:sparkles:] 찜버튼 파이어베이스 연동하여 기능 추가

---
+++ src/components/Detail/LikeBtn.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { selectedMovieProps } from '../../redux/slice/detailSlice'
+
+import { setDoc, doc, deleteDoc, getDoc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { dbService } from '../../firebase-config'
+
+type LikeBtnProps = {
+  movieObj: selectedMovieProps
+}
+
+const LikeBtn: React.FC<LikeBtnProps> = ({ movieObj }) => {
+  const auth = getAuth()
+  const [like, setLike] = useState<boolean>(false)
+
+  const addBtn = async () => {
+    setLike(true)
+    if (auth.currentUser) {
+      await setDoc(
+        doc(dbService, auth.currentUser.uid, movieObj.title),
+        movieObj,
+      )
+      console.log('추가')
+    }
+  }
+  const deleteBtn = async () => {
+    setLike(false)
+    if (auth.currentUser) {
+      await deleteDoc(doc(dbService, auth.currentUser.uid, movieObj.title))
+      console.log('삭제')
+    }
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center gap-2">
+        {like ? (
+          <button onClick={deleteBtn}>
+            <img
+              width={35}
+              src="/src/assets/CheckBtn.svg"
+              alt="찜한 상태 버튼"
+            />
+          </button>
+        ) : (
+          <button onClick={addBtn}>
+            <img
+              width={35}
+              src="/src/assets/EmptyBtn.svg"
+              alt="찜하지 않은 상태 버튼"
+            />
+          </button>
+        )}
+        <p className="text-sm text-neutral-400 ">찜한 콘텐츠</p>
+      </div>
+    </>
+  )
+}
+
+export default LikeBtn
