763cc26c8e24f5befe4553fd02240a4ca87a3ad5
Butter on Bread <qorbaxk97@gmail.com>
2023-05-23 21:51:24 +0900

    [:sparkles:] 사용자 닉네임 변경 할 수 있도록 인풋태그로 수정

--- src/components/Mypage/EditProfile.tsx
+++ src/components/Mypage/EditProfile.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EditImage from './EditImage'
-import { auth } from '../../firebase-config'
+import { RootState } from '../../redux/store'
+import { useSelector } from 'react-redux'
 
-const EditProfile:React.FC = () => {
-  const user = auth.currentUser
+const EditProfile: React.FC = () => {
+  const users = useSelector((userState: RootState) => userState.lg.userGroup)
+  const [text, setText] = useState(users.name)
 
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4 w-96">
       <h2 className="a11y-hidden">유저 정보</h2>
       <EditImage />
-      <p className="text-4xl font-bold">{user?.displayName}</p>
+      <div className="flex flex-row gap-4 items-end justify-between w-full">
+        <span className="text-xs  border-b-[1px] border-white border-solid p-2">
+          닉네임
+        </span>
+        <input
+          type="text"
+          className="text-2xl font-bold bg-black border-b-[1px] border-white border-solid p-2 grow"
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+      </div>
+      <button className="cursor-pointer w-full h-14 bg-neutral-800 hover:bg-primary-100">
+        수정하기
+      </button>
     </div>
   )
 }
