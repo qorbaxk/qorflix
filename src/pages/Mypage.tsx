d6a1b9e749dd6aed4d90ec890f0ecfc7875cd54e
Butter on Bread <qorbaxk97@gmail.com>
2023-05-23 21:50:38 +0900

    [:lipstick:] 마이페이지 레이아웃 조정

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -5,10 +5,14 @@ import EditProfile from './../components/Mypage/EditProfile'
 
 const Mypage: React.FC = () => {
   return (
-    <div className="baseColor baseContainer flex flex-col items-center gap-4">
-      <EditProfile />
-      <Logout />
-      <Unsubscribing />
+    <div className="baseColor baseContainer flex flex-col items-center justify-around">
+      <div>
+        <EditProfile />
+      </div>
+      <div className='flex flex-col gap-4'>
+        <Logout />
+        <Unsubscribing />
+      </div>
     </div>
   )
 }
