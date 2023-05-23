d7a78a36f3cb827ef7369e865331bfd7497e25c2
Butter on Bread <qorbaxk97@gmail.com>
2023-05-23 17:47:54 +0900

    [:sparkles:] 검색어 초기화

--- src/redux/slice/searchSlice.tsx
+++ src/redux/slice/searchSlice.tsx
@@ -15,8 +15,11 @@ export const searchSlice = createSlice({
     getSearch: (state, action) => {
       state.search = action.payload
     },
+    getSearchReset: state => {
+      state.search = initialState.search
+    },
   },
 })
 
-export const { getSearch } = searchSlice.actions
+export const { getSearch, getSearchReset } = searchSlice.actions
 export default searchSlice.reducer
