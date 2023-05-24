94025cd39401c97d6de033c7be9d041523d8e732
Butter on Bread <qorbaxk97@gmail.com>
2023-05-24 15:41:43 +0900

    [:sparkles:] 프로필 정보 변경시 FB에도 적용

--- src/components/Mypage/EditProfile.tsx
+++ src/components/Mypage/EditProfile.tsx
@@ -4,12 +4,14 @@ import { RootState } from '../../redux/store'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { getUserGroup } from '../../redux/slice/userSlice'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+import { storageService } from '../../firebase-config'
 
 const EditProfile: React.FC = () => {
   const auth = getAuth()
   const dispatch = useDispatch()
   const users = useSelector((userState: RootState) => userState.lg.userGroup)
-  const [text, setText] = useState(users.name)
+  const [text, setText] = useState(auth.currentUser?.displayName as string)
 
   const goEdit = async () => {
     if (auth.currentUser && auth.currentUser.displayName !== text) {
@@ -22,6 +24,29 @@ const EditProfile: React.FC = () => {
           name: text,
         }),
       )
+    }
+
+    if (auth.currentUser && auth.currentUser.photoURL !== users.photoURL) {
+      const imgRef = ref(
+        storageService,
+        `userImg/${auth.currentUser.uid}/${users.photoName}`,
+      )
+      const response = await uploadString(imgRef, users.photoURL, 'data_url')
+      const imgUrl = await getDownloadURL(response.ref)
+
+      Promise.all([response, imgUrl]).then(async imgUrl => {
+        if (auth.currentUser) {
+          await updateProfile(auth.currentUser, {
+            photoURL: imgUrl[1],
+          })
+          dispatch(
+            getUserGroup({
+              ...users,
+              photoURL: imgUrl[1],
+            }),
+          )
+        }
+      })
     }
   }
 
