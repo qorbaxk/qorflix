f5411e916d9c5486ccbab273770bf026c188e572
Butter on Bread <qorbaxk97@gmail.com>
2023-05-24 15:41:12 +0900

    [:sparkles:] 프로필 사진 변경시 바뀐 사진으로 렌더링

--- src/components/Mypage/EditImage.tsx
+++ src/components/Mypage/EditImage.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { auth } from '../../firebase-config'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
+import { getUserGroup } from '../../redux/slice/userSlice'
 
 const EditImage: React.FC = () => {
   const user = auth.currentUser
 
-  const onUpdatePhoto = () => {
-    console.log('업데이트')
+  const userGroup = useSelector(
+    (userState: RootState) => userState.lg.userGroup,
+  )
+  const { photoURL } = useSelector(
+    (userState: RootState) => userState.lg.userGroup,
+  )
+
+  const dispatch = useDispatch()
+  const [photo, setPhoto] = useState<string>(user?.photoURL as string)
+  const [fileChange, setFileChange] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (photoURL !== '' && !fileChange) {
+      setPhoto(photoURL)
+      setFileChange(true)
+    } else if (photoURL === '') {
+      setFileChange(true)
+    }
+  }, [photoURL])
+
+  const onUpdatePhoto = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files) {
+      const theProfile = files[0]
+      const reader = new FileReader()
+      reader.onloadend = e => {
+        const { result } = e.target as FileReader
+        if (result) {
+          const data = result as string
+          setPhoto(data)
+          dispatch(
+            getUserGroup({
+              ...userGroup,
+              photoURL: data,
+              photoName: theProfile.name,
+            }),
+          )
+        }
+      }
+      reader.readAsDataURL(theProfile)
+    }
   }
 
   return (
@@ -13,11 +55,11 @@ const EditImage: React.FC = () => {
       <div className="w-32 h-32 relative">
         <label
           htmlFor="file-input"
-          className="border border-4 border-white rounded-full cursor-pointer mb-4 absolute"
+          className="w-32 h-32 border border-4 border-white rounded-full cursor-pointer mb-4 absolute"
         >
           {user?.photoURL && (
             <img
-              src={user.photoURL}
+              src={photo}
               alt="Avatar"
               className="rounded-full w-full h-full object-contain opacity-70"
             />
